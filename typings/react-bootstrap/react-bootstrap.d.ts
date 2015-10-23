0f9a0e2cc7890c6be3fb09b8d7f2e4a0edd42d8b
tomastrajan <tomas.trajan@gmail.com>
2015-10-23 15:56:37 +0200

    chore(dependency): add react-boostrap

---
+++ typings/react-bootstrap/react-bootstrap.d.ts
@@ -0,0 +1,11 @@
+///<reference path='../react/react.d.ts' />
+
+declare module ReactBootstrap {
+    let Button: any;
+    let NavDropdown: any;
+    let MenuItem: any;
+}
+
+declare module 'react-bootstrap' {
+    export = ReactBootstrap;
+}
